bf50217f6394c55a7a55af9cded8521099d7a09c
Ritika Lama <ritikatheeng2002@gmail.com>
2026-09-15 02:22:55 -0500

    changes: few config changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8888, // Development server port
+    strictPort: true, // Force this port, fail if taken
+  },
+  preview: {
+    port: 8888, // Preview server port
+  },
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.png', '**/*.PNG', '**/*.JPG'],
 })
