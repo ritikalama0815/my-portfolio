904ffc99940338952bb35cac6da27541e916a532
Ritika Lama <ritikatheeng2002@gmail.com>
2026-09-15 02:10:27 -0500

    changes: add artwork to gallery

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -27,8 +27,10 @@ import arlecchino from './arlecchino.webp'
 import hyposelenia from './hyposelenia.webp'
 import columbina_sunset from './columbina_sunset.webp'
 import gojo from './gojo.webp'
+import furina from './furina.webp'
 export {
     gojo,
+    furina,
     columbina_sunset,
     hyposelenia,
     arlecchino,
